1828e1a8ba2734c8ee9be417981716391295223c
Erik Luebbers <erluebbers@gmail.com>
2022-02-15 06:58:37 -0800

    commit tree 995a6beea05e8c779804c44c596e0daea9368087

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative './user'
+
+class Student < User
+  attr_accessor :knowledge
+
+  def initialize 
+    @knowledge = []
+  end
+
+  def learn str 
+    @knowledge.push(str)
+    @knowledge
+  end
+  
 
 end
